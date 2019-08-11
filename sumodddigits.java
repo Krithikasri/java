a49dc5c0d1990ccdc7473c9d1d10127021bd6532
Krithikasri <krithisri412@gmail.com>
2019-08-11 19:27:39 +0530

    Create sumodddigits.java

---
+++ sumodddigits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String args[]){
+		int n=0,a=0,r=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		while(n>0){
+			r=n%10;
+			if(r%2!=0){
+				a=a+r;
+			}
+			n=n/10;
+		}
+		System.out.println(a);
+	}
+}
